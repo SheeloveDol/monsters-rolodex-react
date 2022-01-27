4d542d4cf4c1ddf1e10a0acb8f405c7338b61f0d
Sheelove Dol <siesel26@gmail.com>
2022-01-26 22:28:31 -0800

    Add seach bar

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -26,6 +27,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input type="search" placeholder='search here' onChange={e => this.setState({searchField: e.target.value})}/>
         <CardList monsters={this.state.monsters} />
       </div>
     );
