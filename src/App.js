2daf18118cd8be264faed43ac6393c6de63cfc9e
Sheelove Dol <siesel26@gmail.com>
2022-01-26 22:44:07 -0800

    Added filted method for seach bar

--- src/App.js
+++ src/App.js
@@ -25,10 +25,16 @@ class App extends Component {
 
   // This renders our components to the screen 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
+
     return (
       <div className="App">
         <input type="search" placeholder='search here' onChange={e => this.setState({searchField: e.target.value})}/>
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
